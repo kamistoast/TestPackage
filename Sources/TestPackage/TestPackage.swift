599ac30861eac5b2eb452e52f7de39a9aa4e3755
Nico Amann <nico.amann@t-online.de>
2022-10-03 12:30:52 +0200

    test

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public struct TestPackage: View {
+/*public struct TestPackage: View {
     
     var count: Int
 
@@ -13,5 +13,25 @@ public struct TestPackage: View {
         Text("\(count)")
             .bold()
             .foregroundColor(.red)
+    }
+}*/
+
+public struct TestPackage: View {
+    
+    var count: Int
+
+    public init(count: Int) {
+        
+        self.count = count
+    }
+    
+    public var body: some View {
+        ScrollViewReader { proxy in
+            ScrollView {
+                Text("\(count)")
+                    .bold()
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
