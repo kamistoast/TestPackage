34d340507af84d1b2d711f8576a4a8bbe561b0d4
Nico Amann <nico.amann@t-online.de>
2022-11-30 17:27:13 +0100

    l

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -16,15 +16,59 @@ extension TestPackage {
 
 }
 
+
+
+private struct ScrollOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGPoint = .zero
+    
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
+}
+
+struct ScrollView<Content: View>: View {
+    let axes: Axis.Set
+    let showsIndicators: Bool
+    let offsetChanged: (CGPoint) -> Void
+    let content: Content
+
+    init(
+        axes: Axis.Set = .vertical,
+        showsIndicators: Bool = true,
+        offsetChanged: @escaping (CGPoint) -> Void = { _ in },
+        @ViewBuilder content: () -> Content
+    ) {
+        self.axes = axes
+        self.showsIndicators = showsIndicators
+        self.offsetChanged = offsetChanged
+        self.content = content()
+    }
+    
+    var body: some View {
+        SwiftUI.ScrollView(axes, showsIndicators: showsIndicators) {
+            GeometryReader { geometry in
+                Color.clear.preference(
+                    key: ScrollOffsetPreferenceKey.self,
+                    value: geometry.frame(in: .named("scrollView")).origin
+                )
+            }.frame(width: 0, height: 0)
+            content
+        }
+        .coordinateSpace(name: "scrollView")
+        .onPreferenceChange(ScrollOffsetPreferenceKey.self, perform: offsetChanged)
+    }
+}
+
+
+
 extension TestPackage {
     public var body: some View {
         
         ScrollViewReader { proxy in
-            ScrollView {
+            ScrollView(
+                axes: [.horizontal, .vertical],
+                showsIndicators: false,
+                offsetChanged: { print($0) }
+            ) {
                 content
-            }
-            .onChange(of: id) { newValue in
-                print("hil")
             }
             .overlay {
                 Button {
