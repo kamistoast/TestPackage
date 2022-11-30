30816b139be9e0c63bfe5a22e53874457affbc7c
Nico Amann <nico.amann@t-online.de>
2022-11-30 20:10:20 +0100

    h

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -20,54 +20,56 @@ extension TestPackage {
 
 extension TestPackage {
     public var body: some View {
-        
-        ScrollViewReader { proxy in
-            ScrollView(
-                axes: .vertical,
-                showsIndicators: false,
-                offsetChanged: {
+        withAnimation {
+            ScrollViewReader { proxy in
+                ScrollView(
+                    axes: .vertical,
+                    showsIndicators: false,
+                    offsetChanged: {
 
-                    //saves the items position where you scrolled to
-                    if toggle {
-                        savedPosition = $0.y
-                        toggle.toggle()
-                    }
-                    
-                    // makes button visible again
-                    if isVisible == false && toggle == false && savedPosition != $0.y {
-                        withAnimation {
+                        //saves the items position where you scrolled to
+                        if toggle {
+                            savedPosition = $0.y
+                            toggle.toggle()
+                        }
+                        
+                        // makes button visible again
+                        if isVisible == false && toggle == false && savedPosition != $0.y {
+                            
+                                isVisible.toggle()
+                            
+                        }
+                                    
+                    }) {
+                    content
+                }
+                .overlay {
+                    Button {
+                        
+                            proxy.scrollTo(id, anchor: .center)
+                        
                             isVisible.toggle()
+                            toggle.toggle()
+                            
+                        
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.black)
+                                .frame(width: 40, height: 40)
+                            
+                            Image(systemName: "chevron.down")
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
                         }
                     }
-                                
-                }) {
-                content
-            }
-            .overlay {
-                Button {
-                    withAnimation {
-                        proxy.scrollTo(id, anchor: .center)
-                    }
-                        isVisible.toggle()
-                        toggle.toggle()
-                        
-                    
-                } label: {
-                    ZStack {
-                        Circle()
-                            .foregroundColor(.black)
-                            .frame(width: 40, height: 40)
-                        
-                        Image(systemName: "chevron.down")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                    }
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-                .padding(.trailing)
-                .opacity(isVisible ? 1 : 0)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                    .padding(.trailing)
+                    .opacity(isVisible ? 1 : 0)
 
+                }
             }
+
         }
     }
 }
